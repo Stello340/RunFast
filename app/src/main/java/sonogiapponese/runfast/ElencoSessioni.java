425e3a3079458dacdd5fde03eeebf24e292ea4c6
Stefano <s.montisci750@gmail.com>
2017-04-06 00:14:13 +0200

    Aggiunta la Lista

--- app/src/main/java/sonogiapponese/runfast/ElencoSessioni.java
+++ app/src/main/java/sonogiapponese/runfast/ElencoSessioni.java
@@ -3,13 +3,21 @@ package sonogiapponese.runfast;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class ElencoSessioni extends MainActivity {
+    String[] sessionArray = new String[] { "Product1", "Product2", "Product3" };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.elenco_sessioni);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String[] citta = new String[]{"Torino","Roma","Milano","Napoli","Firenze"};
+        Integer[] image = {R.drawable.run50x50, R.drawable.run50x50, R.drawable.run50x50, R.drawable.run50x50, R.drawable.run50x50};
+        CustomList adapter = new CustomList(this, citta, image);
+        ListView list = (ListView)findViewById(R.id.listaSessioni);
+        list.setAdapter(adapter);
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -21,6 +29,4 @@ public class ElencoSessioni extends MainActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-
 }
