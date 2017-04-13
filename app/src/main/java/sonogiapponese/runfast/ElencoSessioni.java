0c8c66cf4d1d2b1967be4c9f50ed1418e0f59e34
Stefano <s.montisci750@gmail.com>
2017-04-13 22:18:05 +0200

    Aggiunta la Lista Sessioni

--- app/src/main/java/sonogiapponese/runfast/ElencoSessioni.java
+++ app/src/main/java/sonogiapponese/runfast/ElencoSessioni.java
@@ -4,20 +4,39 @@ import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ElencoSessioni extends MainActivity {
-    String[] sessionArray = new String[] { "Product1", "Product2", "Product3" };
+    String[] listviewTitle = new String[]{
+            "ListView Title 1", "ListView Title 2", "ListView Title 3", "ListView Title 4",
+            "ListView Title 5", "ListView Title 6", "ListView Title 7", "ListView Title 8"
+    };
+    int listviewImage = R.drawable.run50x50;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.elenco_sessioni);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        String[] citta = new String[]{"Torino","Roma","Milano","Napoli","Firenze"};
-        Integer[] image = {R.drawable.run50x50, R.drawable.run50x50, R.drawable.run50x50, R.drawable.run50x50, R.drawable.run50x50};
-        CustomList adapter = new CustomList(this, citta, image);
-        ListView list = (ListView)findViewById(R.id.listaSessioni);
-        list.setAdapter(adapter);
+        List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
+        for (int i = 0; i < 8; i++) {
+            HashMap<String, String> hmSessioni = new HashMap<String, String>();
+            hmSessioni.put("listview_title", listviewTitle[i]);
+            hmSessioni.put("listview_image", Integer.toString(listviewImage));
+            aList.add(hmSessioni);
+        }
+
+        String[] from = {"listview_image", "listview_title"};
+        int[] to = {R.id.imageList, R.id.textList};
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getBaseContext(), aList, R.layout.element_session_list, from, to);
+        ListView androidListView = (ListView) findViewById(R.id.listaSessioni);
+        androidListView.setAdapter(simpleAdapter);
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
